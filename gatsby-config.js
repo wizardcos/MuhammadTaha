58f4438d6a2db0d23d886f9ed35d62feafa75d35
wizardcos <taharajpoot204@gmail.com>
2024-07-31 17:22:47 +0500

    nothing much

--- gatsby-config.js
+++ gatsby-config.js
@@ -26,7 +26,7 @@ module.exports = {
         background_color: config.colors.darkNavy,
         theme_color: config.colors.navy,
         display: 'minimal-ui',
-        icon: 'src/images/Logo.png',
+        icon: 'src/images/logo.png',
       },
     },
     `gatsby-plugin-offline`,
