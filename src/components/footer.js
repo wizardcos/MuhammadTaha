b5a234f1b9a91e8f7f06b96d0804ead2fc3e1caa
wizardcos <taharajpoot204@gmail.com>
2024-08-12 15:31:44 +0500

    did some changes regarding the footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -106,8 +106,7 @@ const Footer = () => {
 
       <StyledCredit tabindex="-1">
         <a href="https://github.com/bchiang7/v4">
-          <div>Designed &amp; Built by Brittany Chiang</div>
-          <div>Revised by Muhammad Taha Ijaz</div>
+          <div>Designed &amp; Built by Muhammad Taha Ijaz</div>
 
           {githubInfo.stars && githubInfo.forks && (
             <div className="github-stats">
